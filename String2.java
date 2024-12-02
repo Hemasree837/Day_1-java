a0e02cef033852cac4188918eb24fe1eb0c7b732
Hemasree837 <hemasree837@gmail.com>
2024-12-02 16:13:22 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class String2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=String.format("Congractulations %s you won 5 lakhs",a);
+        System.out.println(b);
+    }
+}
