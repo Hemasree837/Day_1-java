3265cae14940353399edd79a8c9b608d05607024
Hemasree837 <hemasree837@gmail.com>
2024-12-02 15:40:48 +0530

    Add files via upload

---
+++ Acceptingstring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AcceptingString
+{
+    public static void main(String args[])
+    {
+        //Accepting a string
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        String c=sc.nextLine();
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+
+    }
+}
