bfc454c1284f46b4575f800ab0802ab9f856f923
Hemasree837 <hemasree837@gmail.com>
2024-12-02 14:38:11 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class datatypes
+{
+    public static void main(String args[])
+    {
+        int a=15;
+        char b='H';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
