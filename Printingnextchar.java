e15f9a45d968611c1a43741d2f4a6a1804e0b61a
Hemasree837 <hemasree837@gmail.com>
2024-12-02 15:18:36 +0530

    Add files via upload

---
+++ Printingnextchar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Printingnextchar
+{
+    public static void main(String args[])
+    {
+        char c='h';
+        int b=c;
+        b+=1;
+        char s=(char)b;
+        System.out.println(s);
+    }
+}
